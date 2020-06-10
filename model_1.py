67e80a7f1c54e9584838c8c41deba1fb9ac6c56a
Ut0pi4 <xiangyao_ng95@hotmail.com>
2020-06-11 03:06:59 +0800

    change aux convolutions

--- model_1.py
+++ model_1.py
@@ -184,17 +184,41 @@ class AuxiliaryConvolutions(nn.Module):
         super(AuxiliaryConvolutions, self).__init__()
 
         # Auxiliary/additional convolutions on top of the VGG base
-        self.conv8_1 = nn.Conv2d(1024, 256, kernel_size=1, padding=0)  # stride = 1, by default
-        self.conv8_2 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1)  # dim. reduction because stride > 1
+        self.conv8 = nn.Sequential(
+        	nn.Conv2d(1024, 256, kernel_size=1, padding=0),
+        	nn.ReLU(),
+        	nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1),
+        	nn.ReLU()
+        	)
+        # self.conv8_1 = nn.Conv2d(1024, 256, kernel_size=1, padding=0)  # stride = 1, by default
+        # self.conv8_2 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1)  # dim. reduction because stride > 1
 
-        self.conv9_1 = nn.Conv2d(512, 128, kernel_size=1, padding=0)
-        self.conv9_2 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)  # dim. reduction because stride > 1
+        self.conv9 = nn.Sequential(
+        	nn.Conv2d(512, 128, kernel_size=1, padding=0),
+        	nn.ReLU(),
+        	nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+        	nn.ReLU()
+        	)
+        # self.conv9_1 = nn.Conv2d(512, 128, kernel_size=1, padding=0)
+        # self.conv9_2 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)  # dim. reduction because stride > 1
 
-        self.conv10_1 = nn.Conv2d(256, 128, kernel_size=1, padding=0)
-        self.conv10_2 = nn.Conv2d(128, 256, kernel_size=3, padding=0)  # dim. reduction because padding = 0
+        self.conv10 = nn.Sequential(
+        	nn.Conv2d(256, 128, kernel_size=1, padding=0),
+        	nn.ReLU(),
+        	nn.Conv2d(128, 256, kernel_size=3, padding=0),
+        	nn.ReLU()
+        	)
+        # self.conv10_1 = nn.Conv2d(256, 128, kernel_size=1, padding=0)
+        # self.conv10_2 = nn.Conv2d(128, 256, kernel_size=3, padding=0)  # dim. reduction because padding = 0
 
-        self.conv11_1 = nn.Conv2d(256, 128, kernel_size=1, padding=0)
-        self.conv11_2 = nn.Conv2d(128, 256, kernel_size=3, padding=0)  # dim. reduction because padding = 0
+        self.conv11 = nn.Sequential(
+        	nn.Conv2d(256, 128, kernel_size=1, padding=0),
+        	nn.ReLU(),
+        	nn.Conv2d(128, 256, kernel_size=3, padding=0),
+        	nn.ReLU()
+        	)
+        # self.conv11_1 = nn.Conv2d(256, 128, kernel_size=1, padding=0)
+        # self.conv11_2 = nn.Conv2d(128, 256, kernel_size=3, padding=0)  # dim. reduction because padding = 0
 
         # Initialize convolutions' parameters
         self.init_conv2d()
@@ -215,20 +239,24 @@ class AuxiliaryConvolutions(nn.Module):
         :param conv7_feats: lower-level conv7 feature map, a tensor of dimensions (N, 1024, 19, 19)
         :return: higher-level feature maps conv8_2, conv9_2, conv10_2, and conv11_2
         """
-        out = F.relu(self.conv8_1(conv7_feats))  # (N, 256, 19, 19)
-        out = F.relu(self.conv8_2(out))  # (N, 512, 10, 10)
-        conv8_2_feats = out  # (N, 512, 10, 10)
+        output = self.conv8(conv7_feats)
+        # out = F.relu(self.conv8_1(conv7_feats))  # (N, 256, 19, 19)
+        # out = F.relu(self.conv8_2(out))  # (N, 512, 10, 10)
+        conv8_2_feats = output  # (N, 512, 10, 10)
 
-        out = F.relu(self.conv9_1(out))  # (N, 128, 10, 10)
-        out = F.relu(self.conv9_2(out))  # (N, 256, 5, 5)
-        conv9_2_feats = out  # (N, 256, 5, 5)
+        output = self.conv9(output)
+        # out = F.relu(self.conv9_1(out))  # (N, 128, 10, 10)
+        # out = F.relu(self.conv9_2(out))  # (N, 256, 5, 5)
+        conv9_2_feats = output  # (N, 256, 5, 5)
 
-        out = F.relu(self.conv10_1(out))  # (N, 128, 5, 5)
-        out = F.relu(self.conv10_2(out))  # (N, 256, 3, 3)
-        conv10_2_feats = out  # (N, 256, 3, 3)
+        output = self.conv10(output)
+        # out = F.relu(self.conv10_1(out))  # (N, 128, 5, 5)
+        # out = F.relu(self.conv10_2(out))  # (N, 256, 3, 3)
+        conv10_2_feats = output  # (N, 256, 3, 3)
 
-        out = F.relu(self.conv11_1(out))  # (N, 128, 3, 3)
-        conv11_2_feats = F.relu(self.conv11_2(out))  # (N, 256, 1, 1)
+        conv11_2_feats = self.conv11(output)
+        # out = F.relu(self.conv11_1(out))  # (N, 128, 3, 3)
+        # conv11_2_feats = F.relu(self.conv11_2(out))  # (N, 256, 1, 1)
 
         # Higher-level feature maps
         return conv8_2_feats, conv9_2_feats, conv10_2_feats, conv11_2_feats
